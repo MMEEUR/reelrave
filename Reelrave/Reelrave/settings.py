baa9aad484f4227424fc6d67c6e7432c07483122
Mahdyar <mmeeur@gmail.com>
2023-03-04 16:37:14 +0000

    Add movies app to INSTALLED_APPS

--- Reelrave/Reelrave/settings.py
+++ Reelrave/Reelrave/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    
+    #Local apps
+    'movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
