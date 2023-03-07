e93db1701a8785dbf4c469def471c06f79e6cf36
Mahdyar <mmeeur@gmail.com>
2023-03-07 17:57:35 +0000

    Create person models

--- Reelrave/persons/models.py
+++ Reelrave/persons/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    ROLES = (('Director', 'Director'), ('Actor', 'Actor'), ('Writer', 'Writer'))
+    
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=8, choices=ROLES)
+    birthday = models.DateField()
+    height_centimeter = models.DecimalField(max_digits=3, decimal_places=0)
+    picture = models.ImageField(upload_to="actors/")
+    
+    def __str__(self):
+        return self.name
