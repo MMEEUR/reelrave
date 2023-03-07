bc05b73171e2b242d287b40425917d71fc917ba8
Mahdyar <mmeeur@gmail.com>
2023-03-07 17:57:06 +0000

    Create movie models

--- Reelrave/movies/models.py
+++ Reelrave/movies/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.validators import FileExtensionValidator
+from persons.models import Person
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.CharField(max_length=20, unique=True, editable=False)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True, editable=False)
+    flag = models.ImageField(upload_to='movies/countries/')
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    RATINGS = (('G', 'G'), ('PG', 'PG'), ('PG-13', 'PG-13'), ('R', 'R'), ('NC-17', 'NC-17'))
+    
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True, editable=False)
+    release_date = models.DateTimeField()
+    time = models.DurationField()
+    content_rating = models.CharField(max_length=5, choices=RATINGS)
+    genre = models.ManyToManyField(Genre, related_name='genre_movies')
+    director = models.ManyToManyField(Person, related_name='director_movies')
+    writers = models.ManyToManyField(Person, related_name='writer_movies')
+    actors = models.ManyToManyField(Person, related_name='actor_movies')
+    description = models.CharField(max_length=250)
+    storyline = models.TextField()
+    country_of_origin = models.ManyToManyField(Country, related_name='movies')
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
+    
+class Picture(models.Model):
+    def get_image_filename(instance, filename):
+        return f"movies/{instance.movies.name}/pictures/{filename}"
+    
+    title = models.CharField(max_length=30)
+    movie = models.ForeignKey(Movie, related_name='pictures', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=get_image_filename)
+    released = models.DateTimeField(auto_now_add=True, editable=False)
+    
+    def __str__(self):
+        return self.title
+
+class Trailer(models.Model):
+    def get_video_filename(instance, filename):
+        return f"movies/{instance.movies.name}/trailers/{filename}"
+    
+    title = models.CharField(max_length=30)
+    movie = models.ForeignKey(Movie, related_name='trailers', on_delete=models.CASCADE)
+    video = models.FileField(upload_to=get_video_filename, validators=[FileExtensionValidator(allowed_extensions=['mov','avi','mp4','webm','mkv'])])
+    released = models.DateTimeField(auto_now_add=True, editable=False)
+    
+    def __str__(self):
+        return self.title
