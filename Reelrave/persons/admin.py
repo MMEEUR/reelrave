79d740b35c1cae22503ecde14517295a85de7d40
Mahdyar <mmeeur@gmail.com>
2023-03-08 18:10:41 +0000

    Add person models to admin

--- Reelrave/persons/admin.py
+++ Reelrave/persons/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Person
 
 # Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role', 'birthday', 'height_centimeter')
+    list_filter = ('role',)
+    ordering = ('name',)
+    search_fields = ('name',)
