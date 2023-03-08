adf35a4caf11b2ff2428321ca5d5d2c22099a18a
Mahdyar <mmeeur@gmail.com>
2023-03-08 18:07:32 +0000

    Add movie models to admin

--- Reelrave/movies/admin.py
+++ Reelrave/movies/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Movie, Picture, Trailer
 
 # Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'release_date', 'time', 'content_rating')
+    list_filter = ('release_date', 'genre', 'content_rating')
+    ordering = ('-release_date',)
+    search_fields = ('name', 'director')
+    
+@admin.register(Picture)
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'movie', 'released')
+    list_filter = ('released',)
+    ordering = ('movie', 'released')
+    search_fields = ('title', 'movie')
+    
+@admin.register(Trailer)
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'movie', 'released')
+    list_filter = ('released',)
+    ordering = ('movie', 'released')
+    search_fields = ('title', 'movie')
