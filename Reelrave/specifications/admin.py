8787c4ec17724adad63724a6d1b3f18d7652ce55
Mahdyar <mmeeur@gmail.com>
2023-03-08 18:16:14 +0000

    Add specifications models to admin

--- Reelrave/specifications/admin.py
+++ Reelrave/specifications/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Country, Genre
 
 # Register your models here.
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
+    
+@admin.register(Genre)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
