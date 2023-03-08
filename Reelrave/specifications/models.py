b4360729085f2a9bfc5c7ce5fe306f3f0338c4e9
Mahdyar <mmeeur@gmail.com>
2023-03-08 17:41:04 +0000

    Create genre & country model

--- Reelrave/specifications/models.py
+++ Reelrave/specifications/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.CharField(max_length=20, unique=True, editable=False)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True, editable=False)
+    flag = models.ImageField(upload_to='countries/')
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
