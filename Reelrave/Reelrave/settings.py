ae4c2cb0942a24ef9980be824c15e95b904408c5
Mahdyar <mmeeur@gmail.com>
2023-03-09 18:23:58 +0000

    Comment default DRF settings

--- Reelrave/Reelrave/settings.py
+++ Reelrave/Reelrave/settings.py
@@ -32,9 +32,9 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ]
 }
 
 # Application definition
