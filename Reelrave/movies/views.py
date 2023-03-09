9642861700d106148357d3931d1b9ed45f0f5b67
Mahdyar <mmeeur@gmail.com>
2023-03-09 18:24:54 +0000

    Create first DRF view

--- Reelrave/movies/views.py
+++ Reelrave/movies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Movie
+from .serializers import MovieSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    data = Movie.objects.all()
+    serializer_data = MovieSerializer(data, many=True)
+    return Response(serializer_data.data)
