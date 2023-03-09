678ba2917b036743d419d676b24fdb5898710466
Mahdyar <mmeeur@gmail.com>
2023-03-09 17:13:11 +0000

    Change some models and admin

--- Reelrave/persons/admin.py
+++ Reelrave/persons/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Role
 
 # Register your models here.
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'role', 'birthday', 'height_centimeter')
-    list_filter = ('role',)
+    list_display = ('id', 'name', 'display_roles', 'birthday', 'height_centimeter')
+    list_filter = ('roles',)
     ordering = ('name',)
     search_fields = ('name',)
+    
+    def display_roles(self, obj):
+        return ", ".join([role.role for role in obj.roles.all()])
+    display_roles.short_description = 'Roles'
+    
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('role',)
+    ordering = ('role',)
+    search_fields = ('role',)
+    raw_id_fields = ('person',)
--- Reelrave/persons/models.py
+++ Reelrave/persons/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    ROLES = (('Director', 'Director'), ('Actor', 'Actor'), ('Writer', 'Writer'))
+    def get_image_filename(instance, filename):
+        return f"presons/{instance.name}/pictures/{filename}"
     
     name = models.CharField(max_length=100)
-    role = models.CharField(max_length=8, choices=ROLES)
     birthday = models.DateField()
     height_centimeter = models.DecimalField(max_digits=3, decimal_places=0)
-    picture = models.ImageField(upload_to="actors/")
+    picture = models.ImageField(upload_to=get_image_filename)
     
     def __str__(self):
         return self.name
+    
+class Role(models.Model):  
+    person = models.ManyToManyField(Person, related_name='roles')
+    role = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.role
