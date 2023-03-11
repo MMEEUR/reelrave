fdd87371f230e485650ae37c2881521c13f296ca
Mahdyar <mmeeur@gmail.com>
2023-03-11 18:05:23 +0000

    Add video & photos to movie model

--- Reelrave/movies/models.py
+++ Reelrave/movies/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 from django.utils.text import slugify
-from django.core.validators import FileExtensionValidator
+from django.contrib.contenttypes.fields import GenericRelation
 from persons.models import Person
-from specifications.models import Genre, Country
+from specifications.models import Genre, Country, Picture, Video
+from accounts.models import Comment
 
 # Create your models here.
 class Movie(models.Model):
-    def get_image_filename(instance, filename):
-        return f"movies/{instance.name}/baners/{filename}"
+    def get_baner_filename(instance, filename):
+        return f"shows/{instance.name}/baners/{filename}"
+    
+    def get_trailer_filename(instance, filename):
+        return f"shows/{instance.name}/trailer/{filename}"
     
     RATINGS = (('G', 'G'), ('PG', 'PG'), ('PG-13', 'PG-13'), ('R', 'R'), ('NC-17', 'NC-17'))
     
     name = models.CharField(max_length=100, unique=True)
     slug = models.CharField(max_length=100, unique=True, editable=False)
-    baner = models.ImageField(upload_to=get_image_filename)
+    baner = models.ImageField(upload_to=get_baner_filename)
+    trailer = models.FileField(upload_to=get_trailer_filename)
     release_date = models.DateTimeField()
     time = models.DurationField()
     content_rating = models.CharField(max_length=5, choices=RATINGS)
@@ -24,6 +29,9 @@ class Movie(models.Model):
     description = models.CharField(max_length=250)
     storyline = models.TextField()
     country_of_origin = models.ManyToManyField(Country, related_name='country_movies')
+    pictures = GenericRelation(Picture)
+    videos = GenericRelation(Video)
+    comments = GenericRelation(Comment)
     
     class Meta:
         ordering = ('-release_date',)
@@ -34,27 +42,3 @@ class Movie(models.Model):
         
     def __str__(self):
         return self.name
-    
-class Picture(models.Model):
-    def get_image_filename(instance, filename):
-        return f"movies/{instance.movie.name}/pictures/{filename}"
-    
-    title = models.CharField(max_length=30)
-    movie = models.ForeignKey(Movie, related_name='pictures', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=get_image_filename)
-    released = models.DateTimeField(auto_now_add=True, editable=False)
-    
-    def __str__(self):
-        return self.title
-
-class Trailer(models.Model):
-    def get_video_filename(instance, filename):
-        return f"movies/{instance.movie.name}/trailers/{filename}"
-    
-    title = models.CharField(max_length=30)
-    movie = models.ForeignKey(Movie, related_name='trailers', on_delete=models.CASCADE)
-    video = models.FileField(upload_to=get_video_filename, validators=[FileExtensionValidator(allowed_extensions=['mov','avi','mp4','webm','mkv'])])
-    released = models.DateTimeField(auto_now_add=True, editable=False)
-    
-    def __str__(self):
-        return self.title
