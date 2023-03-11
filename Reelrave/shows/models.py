407ddcd4d838a727f7d2b85b93257214df152135
Mahdyar <mmeeur@gmail.com>
2023-03-11 18:04:18 +0000

    Create show, season, episode models

--- Reelrave/shows/models.py
+++ Reelrave/shows/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.contenttypes.fields import GenericRelation
+from persons.models import Person
+from specifications.models import Genre, Country, Picture, Video
+from accounts.models import Comment
 
 # Create your models here.
+class Show(models.Model):
+    def get_baner_filename(instance, filename):
+        return f"shows/{instance.name}/baners/{filename}"
+    
+    def get_trailer_filename(instance, filename):
+        return f"shows/{instance.name}/trailer/{filename}"
+    
+    RATINGS = (('TV-Y', 'TV-Y'), ('TV-Y7', 'TV-Y7'), ('TV-G', 'TV-G'), ('TV-PG', 'TV-PG'), ('TV-14', 'TV-14'), ('TV-MA', 'TV-MA'))
+    
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True, editable=False)
+    baner = models.ImageField(upload_to=get_baner_filename)
+    trailer = models.FileField(upload_to=get_trailer_filename)
+    release_date = models.DateField()
+    content_rating = models.CharField(max_length=5, choices=RATINGS)
+    genre = models.ManyToManyField(Genre, related_name='genre_movies')
+    director = models.ManyToManyField(Person, related_name='director_movies')
+    writers = models.ManyToManyField(Person, related_name='writer_movies')
+    actors = models.ManyToManyField(Person, related_name='actor_movies')
+    description = models.CharField(max_length=250)
+    storyline = models.TextField()
+    country_of_origin = models.ManyToManyField(Country, related_name='country_movies')
+    pictures = GenericRelation(Picture)
+    videos = GenericRelation(Video)
+    comments = GenericRelation(Comment)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self) -> str:
+        return self.name
+        
+class Season(models.Model):
+    show = models.ForeignKey(Show, on_delete=models.CASCADE, related_name='seasons')
+    number = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = ('show', 'number')
+        ordering = ['number']
+        
+    def __str__(self) -> str:
+        return f"\"{self.Show}\" Season {self.number}"
+    
+class Episode(models.Model):
+    def get_baner_filename(instance, filename):
+        return f"shows/{instance.season.show}/Season {instance.season.number}/Episode-{instance.number}/baner/{filename}"
+    
+    def get_trailer_filename(instance, filename):
+        return f"shows/{instance.season.show}/Season {instance.season.number}/Episode-{instance.number}/trailer/{filename}"
+    
+    season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name="episodes")
+    number = models.PositiveSmallIntegerField()
+    title = models.CharField(max_length=100)
+    baner = models.ImageField(upload_to=get_baner_filename)
+    trailer = models.FileField(upload_to=get_trailer_filename) 
+    description = models.CharField(max_length=250)
+    released = models.DateField() 
+    pictures = GenericRelation(Picture)
+    videos = GenericRelation(Video)
+    
+    class Meta:
+        unique_together = ('show', 'number')
+        ordering = ['number']
+        
+    def __str__(self) -> str:
+        return f"Episode-{self.number} \"{self.title}\""
