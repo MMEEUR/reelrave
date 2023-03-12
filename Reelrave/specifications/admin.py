b7ce1133788eed1adc2cfa20be381a8c5cf1fef6
Mahdyar <mmeeur@gmail.com>
2023-03-12 20:37:17 +0000

    Create custom form for video and photo

--- Reelrave/specifications/admin.py
+++ Reelrave/specifications/admin.py
@@ -1,7 +1,44 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.admin.widgets import ForeignKeyRawIdWidget
 from .models import Country, Genre, Photo, Video
 
-# Register your models here.
+class MediaForm(forms.ModelForm):
+    content_type = forms.ModelChoiceField(
+        queryset=ContentType.objects.filter(model__in=['show', 'movie', 'episode'])
+    )
+    content_object_id = forms.IntegerField(widget=ForeignKeyRawIdWidget(ContentType.objects.all(), admin_site=admin.site))
+
+    class Meta:
+        abstract = True
+
+class PhotoForm(MediaForm):
+    class Meta:
+        model = Photo
+        fields = ['title', 'content_type', 'content_object_id', 'image']
+
+class VideoForm(MediaForm):
+    class Meta:
+        model = Video
+        fields = ['title', 'content_type', 'content_object_id', 'video']
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    form = PhotoForm
+    list_display = ('title', 'content_object', 'released')
+    list_filter = ('released',)
+    ordering = ('-released',)
+    search_fields = ('title', 'content_object')
+    
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    form = VideoForm 
+    list_display = ('title', 'content_object', 'released')
+    list_filter = ('released',)
+    ordering = ('-released',)
+    search_fields = ('title', 'content_object')
+    
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -13,17 +50,3 @@ class GenreAdmin(admin.ModelAdmin):
     list_display = ('name',)
     ordering = ('name',)
     search_fields = ('name',)
-    
-@admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content_object', 'released')
-    list_filter = ('released',)
-    ordering = ('-released',)
-    search_fields = ('title', 'content_object')
-    
-@admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content_object', 'released')
-    list_filter = ('released',)
-    ordering = ('-released',)
-    search_fields = ('title', 'content_object')
