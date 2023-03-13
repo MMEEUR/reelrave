370d0fe6c03260ccc7119df469c67216481a3a4a
Mahdyar <mmeeur@gmail.com>
2023-03-13 15:22:37 +0000

    Move ManyToMany field to person class

--- Reelrave/persons/models.py
+++ Reelrave/persons/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Role(models.Model):  
+    role = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.role
+    
 class Person(models.Model):
     def get_image_filename(instance, filename):
         return f"presons/{instance.name}/pictures/{filename}"
@@ -9,13 +15,7 @@ class Person(models.Model):
     birthday = models.DateField()
     height_centimeter = models.DecimalField(max_digits=3, decimal_places=0)
     picture = models.ImageField(upload_to=get_image_filename)
+    roles = models.ManyToManyField(Role, related_name='persons')
     
     def __str__(self):
         return self.name
-    
-class Role(models.Model):  
-    persons = models.ManyToManyField(Person, related_name='roles')
-    role = models.CharField(max_length=20)
-    
-    def __str__(self):
-        return self.role
