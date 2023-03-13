93a849291d761e08efa1dcd47da64883ad4a42ea
Mahdyar <mmeeur@gmail.com>
2023-03-13 16:32:11 +0000

    Add comment class to admin

--- Reelrave/specifications/admin.py
+++ Reelrave/specifications/admin.py
@@ -1,43 +1,26 @@
 from django.contrib import admin
-from django import forms
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.admin.widgets import ForeignKeyRawIdWidget
-from .models import Country, Genre, Photo, Video
-
-class MediaForm(forms.ModelForm):
-    content_type = forms.ModelChoiceField(
-        queryset=ContentType.objects.filter(model__in=['show', 'movie', 'episode'])
-    )
-    content_object_id = forms.IntegerField(widget=ForeignKeyRawIdWidget(ContentType.objects.all(), admin_site=admin.site))
-
-    class Meta:
-        abstract = True
-
-class PhotoForm(MediaForm):
-    class Meta:
-        model = Photo
-        fields = ['title', 'content_type', 'content_object_id', 'image']
-
-class VideoForm(MediaForm):
-    class Meta:
-        model = Video
-        fields = ['title', 'content_type', 'content_object_id', 'video']
+from .models import Country, Genre, Photo, Video, Comment
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    form = PhotoForm
     list_display = ('title', 'content_object', 'released')
     list_filter = ('released',)
     ordering = ('-released',)
     search_fields = ('title', 'content_object')
     
 @admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
-    form = VideoForm 
+class VideoAdmin(admin.ModelAdmin): 
     list_display = ('title', 'content_object', 'released')
     list_filter = ('released',)
     ordering = ('-released',)
     search_fields = ('title', 'content_object')
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content_object', 'created', 'active')
+    list_filter = ('active', 'created')
+    ordering = ('active', '-created')
+    search_fields = ('user', 'body', 'content_object')
     
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
