c880350cde0a593f1063bc89da1dff61964b0dd6
Mahdyar <mmeeur@gmail.com>
2023-03-14 18:25:36 +0000

    Add profile to admin

--- Reelrave/accounts/admin.py
+++ Reelrave/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+@admin.register(Profile)
+class ProflieAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_of_birth', 'comments_count')
+    ordering = ('user',)
+    search_fields = ('user',)
+    
+    def comments_count(self, obj):
+        return obj.user_comments.count()
+    comments_count.short_description = 'Comments'
