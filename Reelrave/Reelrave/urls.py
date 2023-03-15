60b23d768b7545d36ce09bea3da4339cf6eefc66
Mahdyar <mmeeur@gmail.com>
2023-03-15 16:48:40 +0000

    Set url for specifications app

--- Reelrave/Reelrave/urls.py
+++ Reelrave/Reelrave/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls', namespace='movies'))
+    path('movies/', include('movies.urls', namespace='movies')),
+    path('', include('specifications.urls', namespace='spec'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
