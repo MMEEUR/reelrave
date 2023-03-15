fd12514a82f1a6fe994897d58a7efa4dc16eb67f
Mahdyar <mmeeur@gmail.com>
2023-03-15 16:51:29 +0000

    Set url for movie comments

--- Reelrave/movies/urls.py
+++ Reelrave/movies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import movie_list
+from .views import movie_list, movie_detail, MovieCommentView
 
 app_name = 'movies'
 
 urlpatterns = [
-    path('', movie_list, name='movie_list')
+    path('', movie_list, name='movie_list'),
+    path('<slug:slug>/', movie_detail, name='movie_detail'),
+    path('<slug:slug>/comments/', MovieCommentView.as_view(), name='movie_detail'),
 ]
