0bc0c59fe8a08c6a06ef10ae01d9d57234de6a63
Mahdyar <mmeeur@gmail.com>
2023-03-16 15:37:10 +0000

    Create genre & country serializer

--- Reelrave/specifications/serializers.py
+++ Reelrave/specifications/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Comment
+from .models import Comment, Genre, Country
+from accounts.serializers import UserCommentSerializer
 
 class CommentSerializer(ModelSerializer):
+    user_profile = UserCommentSerializer(read_only=True)
+    
     class Meta:
         model = Comment
-        exclude = ('id', 'active')
+        exclude = ('id', 'active', 'content_type', 'object_id')
+        
+class GenreSerializer(ModelSerializer):
+    class Meta:
+        model = Genre
+        exclude = ('id',)
+        
+class CountrySeralizer(ModelSerializer):
+    class Meta:
+        model = Country
+        exclude = ('id', 'flag')
