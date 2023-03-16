631b635127daddad8c59db8e1f174b2ffc0c2003
Mahdyar <mmeeur@gmail.com>
2023-03-16 15:46:50 +0000

    Create movie list & detail serializer

--- Reelrave/movies/serializers.py
+++ Reelrave/movies/serializers.py
@@ -1,7 +1,20 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import Movie
+from persons.serializers import PersonSerializer
+from specifications.serializers import CommentSerializer, GenreSerializer, CountrySeralizer
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieDetailSerializer(ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Movie
-        fields = "__all__"
+        exclude = ('id',)
+        
+class MovieListSerializer(ModelSerializer):
+    genre = GenreSerializer(many=True, read_only=True)
+    country_of_origin = CountrySeralizer(many=True, read_only=True)
+    director = PersonSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Movie
+        fields = ('name', 'slug', 'baner', 'release_date', 'time', 'genre', 'director', 'description', 'country_of_origin')
