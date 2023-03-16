181e3892d4e1b7686e67b5ca558dd0facbb42352
Mahdyar <mmeeur@gmail.com>
2023-03-16 14:04:27 +0000

    Add related name to user field

--- Reelrave/accounts/models.py
+++ Reelrave/accounts/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     def get_image_filename(instance, filename):
         return f"users/{instance.user.username}/photos/{filename}"
     
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
     bio = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     photo = models.ImageField(upload_to=get_image_filename)
