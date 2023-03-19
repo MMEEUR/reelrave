79dfd0a02ae11607f94b4e7d292db8da245a9b8a
Mahdyar <mmeeur@gmail.com>
2023-03-19 17:15:54 +0000

    Create register serializer

--- Reelrave/accounts/serializers.py
+++ Reelrave/accounts/serializers.py
@@ -1,20 +1,38 @@
 from django.contrib.auth import get_user_model
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Profile
+        
+class CreateUserSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password', 'confirm_password')
+        extra_kwargs = {'password': {'write_only': True}}
 
-class UserSerializer(ModelSerializer):
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
+    
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('username',)
 
-class UserProfileSerializer(ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     
     class Meta:
         model = Profile
         exclude = ('id',)
         
-class UserCommentSerializer(ModelSerializer):
+class UserCommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     
     class Meta:
