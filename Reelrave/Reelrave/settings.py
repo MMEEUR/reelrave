e18d2d4dfb79a6121dd2b4c56067085069f2cb75
Mahdyar <mmeeur@gmail.com>
2023-03-19 17:14:01 +0000

    Add email auth to AUTHENTICATION_BACKENDS

--- Reelrave/Reelrave/settings.py
+++ Reelrave/Reelrave/settings.py
@@ -143,3 +143,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.auth.EmailAuthBackend'
+]
