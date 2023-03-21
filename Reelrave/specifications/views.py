b28c70327c02c0f6580aa864aed646f3e06ccb20
Mahdyar <mmeeur@gmail.com>
2023-03-21 09:12:40 +0000

    Create country list & detail view

--- Reelrave/specifications/views.py
+++ Reelrave/specifications/views.py
@@ -24,6 +24,31 @@ class GenreDetailView(APIView):
         serializer2 = ShowListSerializer(shows, many=True)
         
         data = {
+            'genre': genre.name,
+            'movies': serializer1.data,
+            'shows': serializer2.data
+        }
+        
+        return Response(data)
+    
+class CountryListView(APIView):
+    def get(self, request):
+        countries = Country.objects.all()
+        serializer = CountrySeralizer(countries, many=True)
+        
+        return Response(serializer.data)
+    
+class CountryDetailView(APIView):
+    def get(self, request, slug):
+        country = get_object_or_404(Country, slug=slug)
+        movies = country.country_movies.all()
+        shows = country.country_shows.all()
+        
+        serializer1 = MovieListSerializer(movies, many=True)
+        serializer2 = ShowListSerializer(shows, many=True)
+        
+        data = {
+            'country': country.name,
             'movies': serializer1.data,
             'shows': serializer2.data
         }
