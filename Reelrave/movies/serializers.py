8f5ea68b100f6144d352669a526e3e8ae30a17e8
Mahdyar <mmeeur@gmail.com>
2023-03-21 08:58:20 +0000

    Update movielistserializer

--- Reelrave/movies/serializers.py
+++ Reelrave/movies/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Movie
 from persons.serializers import PersonSerializer
-from specifications.serializers import CommentSerializer, GenreSerializer, CountrySeralizer
+from specifications.serializers import CommentSerializer, GenreSerializer, CountrySeralizer, PhotoSerializer, VideoSerializer
 
 class MovieDetailSerializer(ModelSerializer):
+    genre = GenreSerializer(many=True, read_only=True)
+    country_of_origin = CountrySeralizer(many=True, read_only=True)
+    director = PersonSerializer(many=True, read_only=True)
+    writers = PersonSerializer(many=True, read_only=True)
+    actors = PersonSerializer(many=True, read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
+    pictures = PhotoSerializer(many=True, read_only=True)
+    videos = VideoSerializer(many=True, read_only=True)
     
     class Meta:
         model = Movie
