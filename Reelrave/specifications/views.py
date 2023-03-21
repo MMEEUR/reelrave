65029d583d9e5fc1546c1ebc760d2fefc4ca9ae7
Mahdyar <mmeeur@gmail.com>
2023-03-21 09:00:10 +0000

    Create genre list & detail view

--- Reelrave/specifications/views.py
+++ Reelrave/specifications/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Genre, Country
+from .serializers import GenreSerializer, CountrySeralizer
+from movies.serializers import MovieListSerializer
+from shows.serializers import ShowListSerializer
 # Create your views here.
+
+class GenreListView(APIView):
+    def get(self, request):
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        
+        return Response(serializer.data)
+    
+class GenreDetailView(APIView):
+    def get(self, request, slug):
+        genre = get_object_or_404(Genre, slug=slug)
+        movies = genre.genre_movies.all()
+        shows = genre.genre_shows.all()
+        
+        serializer1 = MovieListSerializer(movies, many=True)
+        serializer2 = ShowListSerializer(shows, many=True)
+        
+        data = {
+            'movies': serializer1.data,
+            'shows': serializer2.data
+        }
+        
+        return Response(data)
