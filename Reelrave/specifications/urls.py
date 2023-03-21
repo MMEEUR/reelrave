716189d7cd7d99fb165aba9f2eb917a35474701e
Mahdyar <mmeeur@gmail.com>
2023-03-21 09:13:11 +0000

    Set url for country views

--- Reelrave/specifications/urls.py
+++ Reelrave/specifications/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import GenreDetailView, GenreListView
+from .views import GenreDetailView, GenreListView, CountryListView, CountryDetailView
 
 app_name = 'spec'
 
 urlpatterns = [
     path('genres/', GenreListView.as_view(), name='genres'),
     path('genres/<slug:slug>/', GenreDetailView.as_view(), name='genre_detail'),
+    path('countries/', CountryListView.as_view(), name='countries'),
+    path('countries/<slug:slug>/', CountryDetailView.as_view(), name='country_detail'),
 ]
