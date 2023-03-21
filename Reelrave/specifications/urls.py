ff5ad7861b99a0654af01e8179b73ecba85f3e93
Mahdyar <mmeeur@gmail.com>
2023-03-21 09:00:57 +0000

    Set path for genre list & detail

--- Reelrave/specifications/urls.py
+++ Reelrave/specifications/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import GenreDetailView, GenreListView
 
 app_name = 'spec'
 
 urlpatterns = [
-
+    path('genres/', GenreListView.as_view(), name='genres'),
+    path('genres/<slug:slug>/', GenreDetailView.as_view(), name='genre_detail'),
 ]
