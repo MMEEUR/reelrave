e93d0190da81a158624743dea0a5ccfee04be242
Mahdyar <mmeeur@gmail.com>
2023-03-22 12:27:35 +0000

    Set url for shows views

--- Reelrave/shows/urls.py
+++ Reelrave/shows/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import ShowListView, ShowDetailView
 
 app_name = 'shows'
 
 urlpatterns = [
-
+    path('', ShowListView.as_view(), name='show_list'),
+    path('<slug:slug>/', ShowDetailView.as_view(), name='show_detail')
 ]
