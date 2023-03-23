8589d0fae235e88fc96522cbe7ada7befa189dcd
Mahdyar <mmeeur@gmail.com>
2023-03-23 13:27:11 +0000

    Create role and person list & detail serializers

--- Reelrave/persons/serializers.py
+++ Reelrave/persons/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Person
+from .models import Person, Role
+
+class RoleSerializer(ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ('role',)
 
 class PersonSerializer(ModelSerializer):
     class Meta:
         model = Person
         fields = ('name', 'picture')
+        
+class PersonListSerializer(ModelSerializer):
+    roles = RoleSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Person
+        fields = ('name', 'picture', 'roles')
+        
+class PersonDetailSerializer(ModelSerializer):
+    roles = RoleSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Person
+        exclude = ('id',)
