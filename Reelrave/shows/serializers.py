2a37c33330475cfc6e8f7eaa6f84d3cba78e5f35
Mahdyar <mmeeur@gmail.com>
2023-03-23 12:16:33 +0000

    Fix shows serializers

--- Reelrave/shows/serializers.py
+++ Reelrave/shows/serializers.py
@@ -3,19 +3,25 @@ from .models import Show, Season, Episode
 from specifications.serializers import GenreSerializer, CountrySeralizer, PhotoSerializer, VideoSerializer, CommentSerializer
 from persons.serializers import PersonSerializer
 
-class SeasonSerializer(ModelSerializer):
-    class Meta:
-        model = Season
-        exclude = ('id')
-        
-class EpisodeSerializer(ModelSerializer):
-    season = SeasonSerializer(read_only=True)
+class EpisodeDetailSerializer(ModelSerializer):
     pictures = PhotoSerializer(many=True, read_only=True)
     videos = VideoSerializer(many=True, read_only=True)
     
     class Meta:
         model = Episode
-        exclude = ('id', )
+        exclude = ('id',)
+        
+class EpisodeListSerializer(ModelSerializer):
+    class Meta:
+        model = Episode
+        fields = ('number', 'time', 'baner', 'released')
+
+class SeasonSerializer(ModelSerializer):
+    episodes = EpisodeListSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Season
+        exclude = ('id', 'show')
         
 class ShowListSerializer(ModelSerializer):
     genre = GenreSerializer(many=True, read_only=True)
@@ -30,8 +36,7 @@ class ShowDetailSerializer(ModelSerializer):
     seasons = SeasonSerializer(many=True, read_only=True)
     genre = GenreSerializer(many=True, read_only=True)
     country_of_origin = CountrySeralizer(many=True, read_only=True)
-    director = PersonSerializer(many=True, read_only=True)
-    writers = PersonSerializer(many=True, read_only=True)
+    creators = PersonSerializer(many=True, read_only=True)
     actors = PersonSerializer(many=True, read_only=True)
     pictures = PhotoSerializer(many=True, read_only=True)
     videos = VideoSerializer(many=True, read_only=True)
