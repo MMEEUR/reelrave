28ffe364509c5b4bc84c669f4eb96ac9f935feb8
Mahdyar <mmeeur@gmail.com>
2023-03-23 13:27:56 +0000

    Create person list & detail view

--- Reelrave/persons/views.py
+++ Reelrave/persons/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Person, Role
+from .serializers import PersonListSerializer, PersonDetailSerializer, RoleSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def person_list(request):
+    persons = Person.objects.all()
+    roles = Role.objects.all()
+
+    data = {
+        "Persons": PersonListSerializer(persons, many=True).data,
+        "Roles": RoleSerializer(roles, many=True).data
+    }
+
+    return Response(data)
+
+@api_view(['GET'])
+def person_detail(request, id):
+    person = get_object_or_404(Person, id=id)
+    serializer = PersonDetailSerializer(person)
+    
+    return Response(serializer.data)
