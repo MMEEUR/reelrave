9224cb04114f704ff57ab61ee275164527637632
Mahdyar <mmeeur@gmail.com>
2023-03-24 13:58:50 +0000

    Set url for create_comment view

--- Reelrave/movies/urls.py
+++ Reelrave/movies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MovieListView, MovieDetailView
+from .views import MovieListView, MovieDetailView, CreateCommentView
 
 app_name = 'movies'
 
 urlpatterns = [
     path('', MovieListView.as_view(), name='movie_list'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
+    path('<slug:slug>/comment/', CreateCommentView.as_view(), name='create_comment'),
 ]
