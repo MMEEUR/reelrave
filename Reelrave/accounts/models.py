6e7c5bfa73bf003699d0324978bbf04fd27a4486
Mahdyar <mmeeur@gmail.com>
2023-03-25 12:44:45 +0000

    Add null=True, blank=True To profile fields

--- Reelrave/accounts/models.py
+++ Reelrave/accounts/models.py
@@ -7,9 +7,9 @@ class Profile(models.Model):
         return f"users/{instance.user.username}/photos/{filename}"
     
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    bio = models.CharField(max_length=50)
-    date_of_birth = models.DateField()
-    photo = models.ImageField(upload_to=get_image_filename)
+    bio = models.CharField(max_length=50, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    photo = models.ImageField(upload_to=get_image_filename, null=True, blank=True)
     
     def __str__(self) -> str:
         return self.user.username
