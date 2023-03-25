ced1c1e7c71f240c7e8b7e1883c6056a64ac2352
Mahdyar <mmeeur@gmail.com>
2023-03-25 13:23:06 +0000

    Set url for profile view

--- Reelrave/accounts/urls.py
+++ Reelrave/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CreateUserView, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import CreateUserView, LoginView, ProfileView
 
 app_name = "account"
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path('register/', CreateUserView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
 ]
