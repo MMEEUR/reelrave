709d8f0e09ee04c068c76ba2d76f834e6db0dd55
Mahdyar <mmeeur@gmail.com>
2023-03-26 17:42:36 +0000

    Move CommentCreateView to spec app and add MovieCreateCommentView

--- Reelrave/movies/views.py
+++ Reelrave/movies/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import get_object_or_404
-from django.contrib.contenttypes.models import ContentType
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_201_CREATED
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from .models import Movie
 from .serializers import MovieListSerializer, MovieDetailSerializer
-from specifications.serializers import CommentCreateSerializer, CommentSerializer
+from specifications.serializers import CommentSerializer
+from specifications.views import CommentCreateView
 
 
 class MovieListView(APIView):
@@ -30,21 +28,6 @@ class MovieDetailView(APIView):
         return Response(data)
 
 
-class CreateCommentView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request, slug):
-        movie = get_object_or_404(Movie, slug=slug)
-        content_type = ContentType.objects.get_for_model(movie)
-
-        # update data with content_type and object_id
-        data = request.data
-        data['user'] = request.user.id
-        data['content_type'] = content_type.id
-        data['object_id'] = movie.id
-
-        serializer = CommentCreateSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=HTTP_201_CREATED)
+class MovieCreateCommentView(CommentCreateView):
+    def get_object(self, slug):
+        return get_object_or_404(Movie, slug=slug)
