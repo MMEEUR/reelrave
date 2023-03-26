d1e047d3e2ed293d047552fd5fe4aea03999c790
Mahdyar <mmeeur@gmail.com>
2023-03-26 16:03:58 +0000

    Add new fields to episode model

--- Reelrave/shows/models.py
+++ Reelrave/shows/models.py
@@ -4,16 +4,23 @@ from django.contrib.contenttypes.fields import GenericRelation
 from persons.models import Person
 from specifications.models import Genre, Country, Photo, Video, Comment
 
-# Create your models here.
+RATINGS = (
+    ('TV-Y', 'TV-Y'),
+    ('TV-Y7', 'TV-Y7'),
+    ('TV-G', 'TV-G'),
+    ('TV-PG', 'TV-PG'),
+    ('TV-14', 'TV-14'),
+    ('TV-MA', 'TV-MA')
+)
+
+
 class Show(models.Model):
     def get_baner_filename(instance, filename):
         return f"shows/{instance.name}/baners/{filename}"
-    
+
     def get_trailer_filename(instance, filename):
         return f"shows/{instance.name}/trailer/{filename}"
-    
-    RATINGS = (('TV-Y', 'TV-Y'), ('TV-Y7', 'TV-Y7'), ('TV-G', 'TV-G'), ('TV-PG', 'TV-PG'), ('TV-14', 'TV-14'), ('TV-MA', 'TV-MA'))
-    
+
     name = models.CharField(max_length=100, unique=True)
     slug = models.CharField(max_length=100, unique=True, editable=False)
     baner = models.ImageField(upload_to=get_baner_filename)
@@ -30,46 +37,55 @@ class Show(models.Model):
     pictures = GenericRelation(Photo)
     videos = GenericRelation(Video)
     comments = GenericRelation(Comment)
-    
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
-        
+
     def __str__(self) -> str:
         return self.name
-        
+
+
 class Season(models.Model):
-    show = models.ForeignKey(Show, on_delete=models.CASCADE, related_name='seasons')
+    show = models.ForeignKey(
+        Show, on_delete=models.CASCADE, related_name='seasons')
     number = models.PositiveSmallIntegerField()
 
     class Meta:
         unique_together = ('show', 'number')
         ordering = ['number']
-        
+
     def __str__(self) -> str:
         return f"\"{self.show}\" Season {self.number}"
-    
+
+
 class Episode(models.Model):
     def get_baner_filename(instance, filename):
         return f"shows/{instance.season.show}/Season {instance.season.number}/Episode-{instance.number}/baner/{filename}"
-    
+
     def get_trailer_filename(instance, filename):
         return f"shows/{instance.season.show}/Season {instance.season.number}/Episode-{instance.number}/trailer/{filename}"
-    
+
     season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name="episodes")
     number = models.PositiveSmallIntegerField()
     title = models.CharField(max_length=100)
+    content_rating = models.CharField(max_length=5, choices=RATINGS)
+    director = models.ManyToManyField(Person, related_name='director_episodes')
+    writers = models.ManyToManyField(Person, related_name='writer_episodes')
+    actors = models.ManyToManyField(Person, related_name='actor_episodes')
+    genre = models.ManyToManyField(Genre, related_name='genre_episodes')
     time = models.DurationField()
     baner = models.ImageField(upload_to=get_baner_filename)
-    trailer = models.FileField(upload_to=get_trailer_filename, null=True, blank=True) 
+    trailer = models.FileField(upload_to=get_trailer_filename, null=True, blank=True)
     description = models.CharField(max_length=250)
-    released = models.DateField() 
+    storyline = models.TextField()
+    released = models.DateField()
     pictures = GenericRelation(Photo)
     videos = GenericRelation(Video)
-    
+
     class Meta:
         unique_together = ('season', 'number')
         ordering = ['number']
-        
+
     def __str__(self) -> str:
         return f"Episode-{self.number} \"{self.title}\""
