2e61c1ef5593f52c8a4eeb99e578eafdabdae0c5
Mahdyar <mmeeur@gmail.com>
2023-03-26 16:12:22 +0000

    Add episode detail view

--- Reelrave/shows/views.py
+++ Reelrave/shows/views.py
@@ -1,33 +1,35 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.contenttypes.models import ContentType
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED
+from rest_framework.status import HTTP_201_CREATED
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import ShowListSerializer, ShowDetailSerializer
+from .serializers import ShowListSerializer, ShowDetailSerializer, EpisodeDetailSerializer
 from specifications.serializers import CommentCreateSerializer, CommentSerializer
-from .models import Show
+from .models import Show, Episode
 
-# Create your views here.
+
 class ShowListView(APIView):
     def get(self, request):
         shows = Show.objects.all()
         serializer = ShowListSerializer(shows, many=True)
-        
+
         return Response(serializer.data)
-    
+
+
 class ShowDetailView(APIView):
     def get(self, request, slug):
         show = get_object_or_404(Show, slug=slug)
         comments = show.comments.filter(active=True)
-        
+
         data = {
             'show': ShowDetailSerializer(show).data,
             'comments': CommentSerializer(comments, many=True).data
         }
-        
+
         return Response(data)
-    
+
+
 class CreateCommentView(APIView):
     permission_classes = (IsAuthenticated,)
 
@@ -46,3 +48,17 @@ class CreateCommentView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=HTTP_201_CREATED)
+    
+    
+class EpisodeDetailView(APIView):
+    def get(self, request, slug, episode_id):
+        episode = get_object_or_404(Episode, id=episode_id)
+        serializer = EpisodeDetailSerializer(episode)
+        
+        data = {
+            "show": f"{episode.season.show.name}",
+            "number": f"S{episode.season.number}.E{episode.number}",
+            "episode": serializer.data
+        }
+        
+        return Response(data)
