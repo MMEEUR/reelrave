6982603ad0d77e6a99778f0d146a999eef6aebe5
Mahdyar <mmeeur@gmail.com>
2023-03-26 16:14:12 +0000

    Set url for episode_detail view

--- Reelrave/shows/urls.py
+++ Reelrave/shows/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ShowListView, ShowDetailView, CreateCommentView
+from .views import ShowListView, ShowDetailView, CreateCommentView, EpisodeDetailView
 
 app_name = 'shows'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', ShowListView.as_view(), name='show_list'),
     path('<slug:slug>/', ShowDetailView.as_view(), name='show_detail'),
     path('<slug:slug>/comment/', CreateCommentView.as_view(), name='create_comment'),
+    path('<slug:slug>/episode/<int:episode_id>/', EpisodeDetailView.as_view(), name='episode_detail'),
 ]
