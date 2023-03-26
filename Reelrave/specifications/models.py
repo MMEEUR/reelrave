3cc249d3810a4a1ea378562aa1d888112ce93633
Mahdyar <mmeeur@gmail.com>
2023-03-26 16:47:28 +0000

    Add episode to content_type of comment model

--- Reelrave/specifications/models.py
+++ Reelrave/specifications/models.py
@@ -40,7 +40,7 @@ class Video(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user_comments')
     body = models.TextField()
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={"model__in": ('show', 'movie')})
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={"model__in": ('show', 'movie', 'episode')})
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     created = models.DateTimeField(auto_now_add=True)
