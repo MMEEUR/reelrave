637bdaffa1b950e752039cbf5ef784ac25615de9
Mahdyar <mmeeur@gmail.com>
2023-03-26 16:38:08 +0000

    Update the url for show create_comment view

--- Reelrave/shows/urls.py
+++ Reelrave/shows/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import ShowListView, ShowDetailView, CreateCommentView, EpisodeDetailView
+from .views import ShowListView, ShowDetailView, ShowCreateCommentView, EpisodeDetailView
 
 app_name = 'shows'
 
 urlpatterns = [
     path('', ShowListView.as_view(), name='show_list'),
     path('<slug:slug>/', ShowDetailView.as_view(), name='show_detail'),
-    path('<slug:slug>/comment/', CreateCommentView.as_view(), name='create_comment'),
+    path('<slug:slug>/comment/', ShowCreateCommentView.as_view(), name='create_comment'),
     path('<slug:slug>/episode/<int:episode_id>/', EpisodeDetailView.as_view(), name='episode_detail'),
 ]
