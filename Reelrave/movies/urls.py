4381421732b6177746bf9c0142ca44fa2f2da98a
Mahdyar <mmeeur@gmail.com>
2023-03-27 19:47:36 +0000

    Set url for movie_create_rating

--- Reelrave/movies/urls.py
+++ Reelrave/movies/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MovieListView, MovieDetailView, MovieCreateCommentView
+from .views import MovieListView, MovieDetailView, MovieCreateCommentView, MovieCreateRatingView
 
 app_name = 'movies'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', MovieListView.as_view(), name='movie_list'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
     path('<slug:slug>/comment/', MovieCreateCommentView.as_view(), name='movie_create_comment'),
+    path('<slug:slug>/rating/', MovieCreateRatingView.as_view(), name='movie_create_rating'),
 ]
