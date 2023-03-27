142aff817ca4c30eac69fdfa63d64e3a50cfcda8
Mahdyar <mmeeur@gmail.com>
2023-03-27 19:17:32 +0000

    Add rating model to admin

--- Reelrave/specifications/admin.py
+++ Reelrave/specifications/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Country, Genre, Photo, Video, Comment
+from .models import Country, Genre, Photo, Video, Comment, Rating
+
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
@@ -7,27 +8,39 @@ class PhotoAdmin(admin.ModelAdmin):
     list_filter = ('released',)
     ordering = ('-released',)
     search_fields = ('title', 'content_object')
-    
+
+
 @admin.register(Video)
-class VideoAdmin(admin.ModelAdmin): 
+class VideoAdmin(admin.ModelAdmin):
     list_display = ('title', 'content_object', 'released')
     list_filter = ('released',)
     ordering = ('-released',)
     search_fields = ('title', 'content_object')
-    
+
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'content_object', 'created', 'active')
     list_filter = ('active', 'created')
     ordering = ('active', '-created')
     search_fields = ('user', 'body', 'content_object')
-    
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content_object', 'rating', 'created', 'updated')
+    list_filter = ('updated',)
+    ordering = ('-updated',)
+    search_fields = ('user', 'content_object')
+
+
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     ordering = ('name',)
     search_fields = ('name',)
-    
+
+
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('name',)
