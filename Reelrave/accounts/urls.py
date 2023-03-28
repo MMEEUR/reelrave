60f657d877b6501f0a9afc6a849b7d9020ea4ba9
Mahdyar <mmeeur@gmail.com>
2023-03-28 14:09:23 +0000

    Set url for global_profile view

--- Reelrave/accounts/urls.py
+++ Reelrave/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import CreateUserView, LoginView, ProfileView
+from .views import CreateUserView, LoginView, ProfileView, GlobalProfileView
 
 app_name = "account"
 
 urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/<int:user_id>/', GlobalProfileView.as_view(), name='global_profile'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('register/', CreateUserView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
