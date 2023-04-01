79809ccd89072d35b6e69278e6deee6d6960b7ab
Mahdyar <mmeeur@gmail.com>
2023-04-01 12:48:25 +0000

    Add @admin.display to PersonAdmin

--- Reelrave/persons/admin.py
+++ Reelrave/persons/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
 from .models import Person, Role
 
-# Register your models here.
+
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('name', 'display_roles', 'birthday', 'height_centimeter')
     list_filter = ('roles',)
     ordering = ('name',)
     search_fields = ('name',)
-    
+
+    @admin.display(description='Roles')
     def display_roles(self, obj):
         return ", ".join([role.role for role in obj.roles.all()])
-    display_roles.short_description = 'Roles'
-    
+
+
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
     list_display = ('role',)
