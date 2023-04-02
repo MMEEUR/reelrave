43121007c115b6e7922854d5116913c885456d74
Mahdyar <mmeeur@gmail.com>
2023-04-02 20:31:55 +0000

    Replace get_absolute_url with slug

--- Reelrave/movies/serializers.py
+++ Reelrave/movies/serializers.py
@@ -3,6 +3,7 @@ from .models import Movie
 from persons.serializers import PersonSerializer
 from specifications.serializers import GenreSerializer, CountrySeralizer, PhotoSerializer, VideoSerializer
 
+
 class MovieDetailSerializer(ModelSerializer):
     genre = GenreSerializer(many=True, read_only=True)
     country_of_origin = CountrySeralizer(many=True, read_only=True)
@@ -13,16 +14,18 @@ class MovieDetailSerializer(ModelSerializer):
     videos = VideoSerializer(many=True, read_only=True)
     average_rating = FloatField()
     total_ratings = IntegerField()
-    
+
     class Meta:
         model = Movie
         exclude = ('id',)
-        
+
+
 class MovieListSerializer(ModelSerializer):
     genre = GenreSerializer(many=True, read_only=True)
     country_of_origin = CountrySeralizer(many=True, read_only=True)
     director = PersonSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Movie
-        fields = ('name', 'slug', 'average_rating', 'baner', 'release_date', 'time', 'genre', 'director', 'description', 'country_of_origin')
+        fields = ('name', 'get_absolute_url', 'average_rating', 'baner', 'release_date',
+                  'time', 'genre', 'director', 'description', 'country_of_origin')
