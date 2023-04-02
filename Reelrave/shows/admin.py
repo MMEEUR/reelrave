a2487490d44eed3406b4291da354c6724cd72a43
Mahdyar <mmeeur@gmail.com>
2023-04-02 20:27:57 +0000

    Update ShowAdmin & EpisodeAdmin

--- Reelrave/shows/admin.py
+++ Reelrave/shows/admin.py
@@ -13,30 +13,19 @@ class VideoInline(GenericTabularInline):
 @admin.register(Show)
 class ShowAdmin(admin.ModelAdmin):
     list_display = ('name', 'display_genres', 'release_date', 'ending_date', 'content_rating', 'season_count', 'episode_count', 'comments_count')
-    list_filter = ('content_rating', 'release_date')
+    list_filter = ('content_rating', 'release_date', 'genre')
     search_fields = ('name', 'director')
     ordering = ('-release_date',)
     raw_id_fields = ('creators', 'actors', 'country_of_origin')
     inlines = [PhotoInline, VideoInline]
-    
-    def season_count(self, obj):
-        return obj.seasons.count()
-    season_count.short_description = 'Seasons'
-    
-    def episode_count(self, obj):
-        episode_count = 0
-        for season in obj.seasons.all():
-            episode_count += season.episodes.count()
-        return episode_count
-    episode_count.short_description = 'Episodes'
 
+    @admin.display(description='Comments')
     def comments_count(self, obj):
         return obj.comments.count()
-    comments_count.short_description = 'Comments'
     
+    @admin.display(description='Genres')
     def display_genres(self, obj):
         return ", ".join([genre.name for genre in obj.genre.all()])
-    display_genres.short_description = 'Genres'
     
 @admin.register(Season)
 class SeasonAdmin(admin.ModelAdmin):
@@ -51,9 +40,9 @@ class SeasonAdmin(admin.ModelAdmin):
 
 @admin.register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
-    list_display = ('season', 'number', 'title', 'time', 'released')
-    list_filter = ('released',)
-    search_fields = ('season', 'title')
-    ordering = ('-released', '-season')
+    list_display = ('season', 'number', 'name', 'time', 'release_date')
+    list_filter = ('release_date',)
+    search_fields = ('season', 'name')
+    ordering = ('-release_date', '-season')
     raw_id_fields = ('season',)
     inlines = [PhotoInline, VideoInline]
