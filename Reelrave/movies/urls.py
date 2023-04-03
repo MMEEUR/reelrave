035c68ec7a68c04b29b1c19211606047a0292b7e
Mahdyar <mmeeur@gmail.com>
2023-04-03 17:18:00 +0000

    Change url name to movie_rating

--- Reelrave/movies/urls.py
+++ Reelrave/movies/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MovieListView, MovieDetailView, MovieCreateCommentView, MovieCreateRatingView, MovieWatchListView
+from .views import MovieListView, MovieDetailView, MovieCreateCommentView, MovieRatingView, MovieWatchListView
 
 app_name = 'movies'
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', MovieListView.as_view(), name='movie_list'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
     path('<slug:slug>/comment/', MovieCreateCommentView.as_view(), name='movie_create_comment'),
-    path('<slug:slug>/rating/', MovieCreateRatingView.as_view(), name='movie_create_rating'),
+    path('<slug:slug>/rating/', MovieRatingView.as_view(), name='movie_rating'),
     path('<slug:slug>/watchlist/', MovieWatchListView.as_view(), name='movie_watchlist'),
 ]
