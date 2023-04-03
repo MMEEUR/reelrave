57fa8611f5da05a39adba4093fc8cf03f1aff324
Mahdyar <mmeeur@gmail.com>
2023-04-03 17:19:01 +0000

    Change url names to show_rating & episode_rating

--- Reelrave/shows/urls.py
+++ Reelrave/shows/urls.py
@@ -3,12 +3,12 @@ from .views import (
     ShowListView,
     ShowDetailView,
     ShowCreateCommentView,
-    ShowCreateRatingView,
+    ShowRatingView,
     ShowWatchListView,
     EpisodeListView,
     EpisodeDetailView,
     EpisodeCreateCommentView,
-    EpisodeCreateRatingView,
+    EpisodeRatingView,
     EpisodeWatchListView,
 )
 
@@ -18,11 +18,11 @@ urlpatterns = [
     path('', ShowListView.as_view(), name='show_list'),
     path('<slug:slug>/', ShowDetailView.as_view(), name='show_detail'),
     path('<slug:slug>/comment/', ShowCreateCommentView.as_view(), name='show_create_comment'),
-    path('<slug:slug>/rating/', ShowCreateRatingView.as_view(), name='show_create_rating'),
+    path('<slug:slug>/rating/', ShowRatingView.as_view(), name='show_rating'),
     path('<slug:slug>/watchlist/', ShowWatchListView.as_view(), name='show_watchlist'),
     path('<slug:slug>/episodes/', EpisodeListView.as_view(), name='episode_list'),
     path('<slug:slug>/episodes/<int:episode_id>/', EpisodeDetailView.as_view(), name='episode_detail'),
     path('<slug:slug>/episodes/<int:episode_id>/comment/', EpisodeCreateCommentView.as_view(), name='episode_create_comment'),
-    path('<slug:slug>/episodes/<int:episode_id>/rating/', EpisodeCreateRatingView.as_view(), name='episode_create_rating'),
+    path('<slug:slug>/episodes/<int:episode_id>/rating/', EpisodeRatingView.as_view(), name='episode_rating'),
     path('<slug:slug>/episodes/<int:episode_id>/watchlist/', EpisodeWatchListView.as_view(), name='episode_watchlist'),
 ]
