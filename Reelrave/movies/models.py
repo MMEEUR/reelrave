28384d431a0324db667c309c159a60a9c2cdfd9c
Mahdyar <mmeeur@gmail.com>
2023-04-05 19:29:58 +0000

    Remove trailer field from movie model

--- Reelrave/movies/models.py
+++ Reelrave/movies/models.py
@@ -19,13 +19,9 @@ class Movie(models.Model):
     def get_baner_filename(instance, filename):
         return f"movies/{instance.name}/baners/{filename}"
     
-    def get_trailer_filename(instance, filename):
-        return f"movies/{instance.name}/trailer/{filename}"
-    
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True, editable=False)
     baner = models.ImageField(upload_to=get_baner_filename)
-    trailer = models.FileField(upload_to=get_trailer_filename, null=True, blank=True)
     release_date = models.DateField()
     time = models.DurationField()
     content_rating = models.CharField(max_length=5, choices=RATINGS)
