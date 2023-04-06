14e74ae450e8cbdc5535176a85b6dbfbc49695d4
Mahdyar <mmeeur@gmail.com>
2023-04-06 21:39:39 +0000

    Remove genre_detail & country_detail urls

--- Reelrave/specifications/urls.py
+++ Reelrave/specifications/urls.py
@@ -9,9 +9,7 @@ app_name = 'spec'
 
 urlpatterns = [
     path('genres/', GenreListView.as_view(), name='genres'),
-    path('genres/<slug:slug>/', GenreDetailView.as_view(), name='genre_detail'),
     path('countries/', CountryListView.as_view(), name='countries'),
-    path('countries/<slug:slug>/', CountryDetailView.as_view(), name='country_detail'),
     path('comment/<int:comment_id>/', UpdateDeleteCommentView.as_view(), name='comment_update_delete'),
     path('comment/<int:comment_id>/like_or_dislike/', CommentLikeDisLikeView.as_view(), name='comment_like_or_dislike'),
 ]
