04f4451cc296e9aa9e269712de9b4337e23e0b23
Mahdyar <mmeeur@gmail.com>
2023-04-06 21:45:32 +0000

    Set url for movie_by_country & movie_by_genre views

--- Reelrave/movies/urls.py
+++ Reelrave/movies/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import MovieListView, MovieDetailView, MovieCreateCommentView, MovieRatingView, MovieWatchListView
+from .views import (
+MovieListView, MovieDetailView, 
+MovieCreateCommentView, MovieRatingView, MovieWatchListView,
+MovieGenreView, MovieCountryView
+)
 
 app_name = 'movies'
 
@@ -9,4 +13,6 @@ urlpatterns = [
     path('<slug:slug>/comment/', MovieCreateCommentView.as_view(), name='movie_create_comment'),
     path('<slug:slug>/rating/', MovieRatingView.as_view(), name='movie_rating'),
     path('<slug:slug>/watchlist/', MovieWatchListView.as_view(), name='movie_watchlist'),
+    path('genre/<slug:genre>/', MovieGenreView.as_view(), name='movie_by_genre'),
+    path('country/<slug:country>/', MovieCountryView.as_view(), name='movie_by_country'),
 ]
