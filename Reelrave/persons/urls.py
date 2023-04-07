95c91f9d9d6dd3ff491f2597b993145e78d9e126
Mahdyar <mmeeur@gmail.com>
2023-04-07 16:53:24 +0000

    Set url for person_list_by_role

--- Reelrave/persons/urls.py
+++ Reelrave/persons/urls.py
@@ -5,5 +5,6 @@ app_name = 'persons'
 
 urlpatterns = [
     path('', person_list, name='person_list'),
+    path('<slug:role>/', person_list, name='person_list_by_role'),
     path('<int:id>/', person_detail, name='person_detail')
 ]
