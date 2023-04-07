80f931beadac8470d597ca1785e72ed4938cb376
Mahdyar <mmeeur@gmail.com>
2023-04-07 16:52:30 +0000

    Add pagination to person_list view

--- Reelrave/persons/views.py
+++ Reelrave/persons/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from .models import Person, Role
 from .serializers import PersonListSerializer, PersonDetailSerializer, RoleSerializer
 
 # Create your views here.
 @api_view(['GET'])
-def person_list(request):
-    persons = Person.objects.all()
-    roles = Role.objects.all()
+def person_list(request, role=None):
+    if role:
+        roles = get_object_or_404(Role, slug=role)
+        role_serializer = RoleSerializer(roles)
+        persons = roles.persons.all()
+        
+    else:
+        roles = Role.objects.all()
+        role_serializer = RoleSerializer(roles, many=True)
+        persons = Person.objects.all()
+
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    page = paginator.paginate_queryset(persons, request)
+    person_serializer = PersonListSerializer(page, many=True)
 
     data = {
-        "Persons": PersonListSerializer(persons, many=True).data,
-        "Roles": RoleSerializer(roles, many=True).data
+        "Roles": role_serializer.data,
+        "Persons": person_serializer.data
     }
 
-    return Response(data)
+    response = Response(data)
+    response['Total-Count'] = paginator.page.paginator.count
+    response['Page-Size'] = paginator.page_size
+    response['Page'] = paginator.page.number
+    
+    return response
 
 @api_view(['GET'])
 def person_detail(request, id):
