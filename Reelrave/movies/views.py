5a8f86a7e7486223cf7bc64dd17c59832434cf16
Mahdyar <mmeeur@gmail.com>
2023-04-09 18:32:56 +0000

    Add TopMoviesView and cache it for 24 hours

--- Reelrave/movies/views.py
+++ Reelrave/movies/views.py
@@ -1,17 +1,54 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Avg, Q, Count
+from django.core.cache import cache
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer
+from specifications.models import Genre
+from .serializers import MovieListSerializer, MovieDetailSerializer, TopMoviesSerializer
 from specifications.serializers import CommentSerializer
 from specifications.views import CommentCreateView, RatingView, WatchListView, GenreDetailView, CountryDetailView
+
+
+class TopMoviesView(APIView):
+    def get(self, request, genre=None):
+        if genre:
+            genre_obj = get_object_or_404(Genre, slug=genre)
+        
+        cache_key = f"top_movies:{genre}"
+        cached_result = cache.get(cache_key)
+        
+        if cached_result is not None:
+            return Response(cached_result)
+        
+        minimum_ratings = 1
+        
+        if genre:
+            top_movies = Movie.objects.filter(genre=genre_obj,)\
+                        .annotate(num_ratings=Count('ratings'))\
+                        .filter(num_ratings__gte=minimum_ratings)\
+                        .annotate(avg_rating=Avg('ratings__rating', filter=~Q(ratings__rating=0)))\
+                        .order_by('-avg_rating')[:250]
+
+        else:
+            top_movies = Movie.objects.annotate(num_ratings=Count('ratings'))\
+                        .filter(num_ratings__gte=minimum_ratings)\
+                        .annotate(avg_rating=Avg('ratings__rating', filter=~Q(ratings__rating=0)))\
+                        .order_by('-avg_rating')[:250]
+        
+        serializer = TopMoviesSerializer(top_movies, many=True)
+        
+        if serializer:
+            cache.set(cache_key, serializer.data, 86400) # cache for 24 hours
+        
+        return Response(serializer.data)
 
 
 class MovieListView(APIView):
     def get(self, request):
         shows = Movie.objects.all()
-        
+
         paginator = PageNumberPagination()
         paginator.page_size = 10
         page = paginator.paginate_queryset(shows, request)
@@ -21,7 +58,7 @@ class MovieListView(APIView):
         response['Total-Count'] = paginator.page.paginator.count
         response['Page-Size'] = paginator.page_size
         response['Page'] = paginator.page.number
-        
+
         return response
 
 
@@ -41,23 +78,23 @@ class MovieDetailView(APIView):
 class MovieCreateCommentView(CommentCreateView):
     def get_object(self, slug):
         return get_object_or_404(Movie, slug=slug)
-    
-    
+
+
 class MovieRatingView(RatingView):
     def get_object(self, slug):
         return get_object_or_404(Movie, slug=slug)
-    
-    
+
+
 class MovieWatchListView(WatchListView):
     def get_object(self, slug):
         return get_object_or_404(Movie, slug=slug)
-    
-    
+
+
 class MovieGenreView(GenreDetailView):
     model = "movies"
     serializer_class = MovieListSerializer
-    
-    
+
+
 class MovieCountryView(CountryDetailView):
     model = "movies"
     serializer_class = MovieListSerializer
