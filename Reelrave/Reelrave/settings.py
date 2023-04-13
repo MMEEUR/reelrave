3250226de7d8cc85cd1e256b46d176d7211b9bd9
Mahdyar <mmeeur@gmail.com>
2023-04-13 21:03:07 +0000

    Change database to postgresql

--- Reelrave/Reelrave/settings.py
+++ Reelrave/Reelrave/settings.py
@@ -96,10 +96,16 @@ WSGI_APPLICATION = 'Reelrave.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Change the postgres settings according to your database
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Reelrave',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
