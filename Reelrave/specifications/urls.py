72308589b1fee699658b8d39dbcfe48c8c70235e
Mahdyar <mmeeur@gmail.com>
2023-05-11 15:19:09 +0000

    Set url for videos & photos views

--- Reelrave/specifications/urls.py
+++ Reelrave/specifications/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from .views import (
-    GenreDetailView, GenreListView,
-    CountryListView, CountryDetailView,
+    VideoListView, PhotoListView,
+    GenreListView, CountryListView, 
     UpdateDeleteCommentView, CommentLikeDisLikeView
 )
 
 app_name = 'spec'
 
 urlpatterns = [
+    path('videos/', VideoListView.as_view(), name='videos'),
+    path('photos/', PhotoListView.as_view(), name='photos'),
     path('genres/', GenreListView.as_view(), name='genres'),
     path('countries/', CountryListView.as_view(), name='countries'),
     path('comment/<int:comment_id>/', UpdateDeleteCommentView.as_view(), name='comment_update_delete'),
