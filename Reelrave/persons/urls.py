bf8cf3484358474f7890d1e4870921abeb1cf896
Mahdyar <mmeeur@gmail.com>
2023-05-11 14:41:24 +0000

    Set url for search view

--- Reelrave/persons/urls.py
+++ Reelrave/persons/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import person_list, person_detail
+from .views import person_list, person_detail, person_search
 
 app_name = 'persons'
 
 urlpatterns = [
     path('', person_list, name='person_list'),
-    path('<slug:role>/', person_list, name='person_list_by_role'),
-    path('<int:id>/', person_detail, name='person_detail')
+    path('<int:id>/', person_detail, name='person_detail'),
+    path('role/<slug:role>/', person_list, name='person_list_by_role'),
+    path('search/', person_search, name='person_search')
 ]
