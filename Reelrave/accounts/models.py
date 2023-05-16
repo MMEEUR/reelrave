1f900f48774299fb9bffda8329ff2efb8d8afc08
Mahdyar <mmeeur@gmail.com>
2023-05-16 14:56:51 +0000

    Change the Profile with a CustomUser model

--- Reelrave/accounts/models.py
+++ Reelrave/accounts/models.py
@@ -1,45 +1,51 @@
 from django.db import models
 from django.urls import reverse
-from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.core.validators import FileExtensionValidator
 from django.core.files.images import ImageFile
 from django.utils.translation import gettext_lazy as _
-from PIL import Image
 
 
-class Profile(models.Model):
+class CustomUser(AbstractUser):
     def get_image_filename(instance, filename):
-        return f"users/{instance.user.username}/photos/{filename}"
+        return f"users/{instance.username}/photos/{filename}"
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    email = models.EmailField(_("email address"))
     bio = models.CharField(max_length=50, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    photo = models.ImageField(upload_to=get_image_filename, null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png'])])
-    
+    photo = models.ImageField(
+        upload_to=get_image_filename,
+        null=True,
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=["jpg", "png"])],
+    )
+
     @property
     def get_absolute_url(self):
-        return reverse("accounts:global_profile", kwargs={"user_id": self.user.id})
-    
+        return reverse("accounts:global_profile", kwargs={"user_id": self.id})
+
     @property
     def comments_count(self):
-        return self.user.user_comments.filter(active=True).count()
+        return self.user_comments.filter(active=True).count()
 
     def __str__(self) -> str:
-        return self.user.username
-    
+        return self.username
+
     def clean(self):
         super().clean()
-        
+
         if self.photo:
             try:
                 image = ImageFile(self.photo)
-                
+
                 if image.width > 1920 or image.height > 1080:
-                    raise ValidationError(_('Image dimensions should not exceed 1920x1080.'))
-                
-                if self.photo.size > 1024*1024*2:
-                    raise ValidationError(_('Image size should not exceed 2 MB.'))
-                
+                    raise ValidationError(
+                        _("Image dimensions should not exceed 1920x1080.")
+                    )
+
+                if self.photo.size > 1024 * 1024 * 2:
+                    raise ValidationError(_("Image size should not exceed 2 MB."))
+
             except OSError:
-                raise ValidationError(_('Invalid image file.')) 
+                raise ValidationError(_("Invalid image file."))
