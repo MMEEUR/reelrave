0438dfd4befd27b43200bd2f811a4906d28c67a2
Mahdyar <mmeeur@gmail.com>
2023-05-16 15:00:35 +0000

    Add & change the default UserAdmin

--- Reelrave/accounts/admin.py
+++ Reelrave/accounts/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Profile
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
-@admin.register(Profile)
-class ProflieAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date_of_birth', 'comments_count')
-    ordering = ('user',)
-    search_fields = ('user',)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ["username", "email", "is_staff", "comments_count"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("bio", "date_of_birth", "photo")}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("bio", "date_of_birth", "photo")}),)
