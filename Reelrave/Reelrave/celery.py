483fb8a755917f458df708bc4bdb290a072f0a1a
Mahdyar <mmeeur@gmail.com>
2023-06-01 16:00:15 +0000

    Add schedule for delete_expired_records task

--- Reelrave/Reelrave/celery.py
+++ Reelrave/Reelrave/celery.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import timedelta
 from celery import Celery
 
 
@@ -7,3 +7,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reelrave.settings')
 app = Celery('Reelrave')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'delete_expired_records': {
+        'task': 'ReelRave.tasks.delete_expired_records',
+        'schedule': timedelta(hours=1),
+    },
+}
