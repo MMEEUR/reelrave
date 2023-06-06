b7e43b0d6f4bb1f9a6c445f83d5ba33f81001be0
Mahdyar <mmeeur@gmail.com>
2023-06-06 16:29:21 +0000

    Create RegisterTest

--- Reelrave/accounts/tests.py
+++ Reelrave/accounts/tests.py
@@ -1,71 +1,40 @@
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import EmailConfirm
 
 
-class AccountTestCase(APITestCase):
+User = get_user_model()
+
+
+class RegisterTest(APITestCase):
     def setUp(self):
-        self.email_code = None
-        self.refresh_token = None
-        self.user_id = None
-        
-        return super().setUp()
-    
-    def test_send_email_code(self):
         url = reverse("accounts:resend_code")
         
         data = {
-            'email': 'testuser@example.com'
+            "email": "test@example.com"
         }
         
         response = self.client.post(url, data)
         
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-        self.email_code = EmailConfirm.objects.get(email='testuser@example.com').code
+        self.email_code = EmailConfirm.objects.get(email="test@example.com").code
         
+        return super().setUp()
     
     def test_register(self):
         url = reverse("accounts:register")
         
         data = {
-            'username': 'testuser',
-            'email': 'testuser@example.com',
-            'password': 'testpassword',
-            'confirm_password': 'testpassword',
-            'code': self.email_code
+            "username": "testuser",
+            "password": "testpassword",
+            "confirm_password": "testpassword",
+            "email": "test@example.com",
+            "email_code": self.email_code
         }
         
         response = self.client.post(url, data)
         
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
-        self.user_id = response.data['id']
-        
-    def test_login(self):
-        url = reverse("accounts:login")
-        
-        data = {
-            'username': 'testuser',
-            'password': 'testpassword',
-        }
-        
-        response = self.client.post(url, data)
-        
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
-        self.refresh_token = response.data['refresh']
-        
-    def test_token_refresh(self):
-        url = reverse("accounts:token_refresh")
-        
-        data = {
-            'refresh': self.refresh_token
-        }
-        
-        response = self.client.post(url, data)
-        
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
-        self.access_token = response.data['access']
