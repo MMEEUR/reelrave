e99181615f6c551696a5c88b54dcf0ae34a389da
Mahdyar <mmeeur@gmail.com>
2023-06-12 13:41:47 +0000

    Add ordering to Meta classes

--- Reelrave/persons/models.py
+++ Reelrave/persons/models.py
@@ -8,6 +8,7 @@ class Role(models.Model):
     slug = models.SlugField(max_length=20, unique=True, editable=False)
     
     class Meta:
+        ordering = ("role",)
         verbose_name_plural = _("Roles")
     
     def __str__(self):
@@ -25,10 +26,11 @@ class Person(models.Model):
     name = models.CharField(max_length=100, verbose_name=_("Name"))
     birthday = models.DateField(verbose_name=_("Birthday"))
     height_centimeter = models.DecimalField(max_digits=3, decimal_places=0, verbose_name=_("Height (cm)"))
-    picture = models.ImageField(upload_to=get_image_filename, verbose_name=_("Picture"))
+    picture = models.ImageField(upload_to=get_image_filename, verbose_name=_("Picture"), null=True, blank=True)
     roles = models.ManyToManyField(Role, related_name='persons', related_query_name='persons')
     
     class Meta:
+        ordering = ("name",)
         verbose_name_plural = _("Persons")
     
     def __str__(self):
