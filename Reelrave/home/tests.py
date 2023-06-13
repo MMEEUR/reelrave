983bb52ecd897616a2a4ce6b21554b1498f49d38
Mahdyar <mmeeur@gmail.com>
2023-06-13 12:02:44 +0000

    Add HomeTest

--- Reelrave/home/tests.py
+++ Reelrave/home/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class HomeTest(APITestCase):
+    def test_home_page(self):
+        data = {
+            "featured_movies": [],
+            "latest_trailers": [],
+            "latest_episodes": [],
+            "latest_movies": [],
+        }
+
+        response = self.client.get("")
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, data)
