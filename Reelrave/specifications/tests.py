5f573b43ca5c045a458cff4972f1462b175c08b9
Mahdyar <mmeeur@gmail.com>
2023-06-22 16:21:31 +0330

    Add GenreTest

--- Reelrave/specifications/tests.py
+++ Reelrave/specifications/tests.py
@@ -5,7 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 from rest_framework import status
 from rest_framework.test import APITestCase
 from movies.models import Movie
-from .models import Comment, Photo, Video
+from .models import Comment, Photo, Video, Genre, Country
 
 
 User = get_user_model()
@@ -69,6 +69,41 @@ class VideoTest(APITestCase):
         url = reverse("spec:videos")
 
         response = self.client.get(url)
-        
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data[0]["title"], self.video.title)
+
+
+class GenreTest(APITestCase):
+    def setUp(self):
+        self.movie = Movie.objects.create(
+            name="TestMovie",
+            release_date="2023-06-20",
+            time="02:22:32",
+            content_rating="R",
+            storyline="test",
+            description="test",
+            featured=True,
+            baner="files/movies/TestMovie/baners/test.jpg",
+        )
+
+        self.genre = Genre.objects.create(name="test")
+        self.genre_2 = Genre.objects.create(name="test2")
+
+        self.movie.genre.add(self.genre)
+        self.movie.genre.add(self.genre_2)
+
+        return super().setUp()
+
+    def test_genres(self):
+        url = reverse("spec:genres")
+
+        response = self.client.get(url)
+
+        print(response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["genre_movies"][0]["name"], self.genre.name)
+        self.assertEqual(response.data["genre_movies"][1]["name"], self.genre_2.name)
+        self.assertEqual(response.data["genre_movies"][0]["movies_count"], 1)
+        self.assertEqual(response.data["genre_movies"][1]["movies_count"], 1)
