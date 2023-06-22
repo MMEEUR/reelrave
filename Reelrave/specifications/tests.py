765bb29ffce45d8d1d57f2c3080c7fa00975c34f
Mahdyar <mmeeur@gmail.com>
2023-06-22 17:34:48 +0330

    Add CommentLikeDisLikeTest

--- Reelrave/specifications/tests.py
+++ Reelrave/specifications/tests.py
@@ -186,3 +186,55 @@ class CommentTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Comment.objects.filter(id=self.comment.id).exists(), False)
+        
+        
+class CommentLikeDisLikeTest(APITestCase):
+    def setUp(self):
+        self.movie = Movie.objects.create(
+            name="TestMovie",
+            release_date="2023-06-20",
+            time="02:22:32",
+            content_rating="R",
+            storyline="test",
+            description="test",
+            featured=True,
+            baner="files/movies/TestMovie/baners/test.jpg",
+        )
+
+        self.user = User.objects.create(
+            username="testuser",
+            email="test@example.com",
+            password=make_password("testpassword"),
+        )
+
+        self.comment = Comment.objects.create(
+            user=self.user,
+            body="Test",
+            content_type=ContentType.objects.get_for_model(self.movie._meta.model),
+            object_id=self.movie.id,
+            active=True,
+        )
+        
+        access_token = RefreshToken.for_user(self.user).access_token
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+        
+        return super().setUp()
+    
+    def test_comment_like_or_dislike(self):
+        url = reverse("spec:comment_like_or_dislike",  kwargs={"comment_id": self.comment.id})
+        
+        response = self.client.post(url, {"like_or_dislike": True}, format="json")
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.comment.likes_count, 1)
+        
+        response = self.client.patch(url, {"like_or_dislike": False}, format="json")
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.comment.comment_likes_dislikes.first().like_or_dislike, False)
+        
+        response = self.client.delete(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(self.comment.comment_likes_dislikes.exists(), False)
