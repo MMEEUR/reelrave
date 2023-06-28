f62ef859553e507cf70458dec25e605eafdbf7be
Mahdyar <mmeeur@gmail.com>
2023-06-28 15:35:14 +0330

    Change baner to ImageField

--- Reelrave/home/serializers.py
+++ Reelrave/home/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework.serializers import Serializer, \
-    CharField, IntegerField, DateField, URLField, SlugField
+    CharField, IntegerField, DateField, URLField, ImageField
 
 
 class SearchContentserializer(Serializer):
     object_id = IntegerField()
     object_type = CharField()
     name = CharField()
-    baner = URLField()
+    baner = ImageField()
     release_date = DateField()
     get_absolute_url = URLField()
