90262b72d3f85c2aff3719c469c5abc0421d1254
Mahdyar Eatemad <mmeeur>
2024-12-18 23:48:44 +0330

    Add npm ip

--- Reelrave/Reelrave/settings.py
+++ Reelrave/Reelrave/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # Third party packages
+    'corsheaders',
     'rest_framework',
     
     # Local apps
@@ -53,6 +54,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,6 +81,22 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+# CORS settings
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8080",
+    "http://localhost:8080"
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
 ]
 
 # Rest framework settings
